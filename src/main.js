db80c5c85a04d24aacacb2089c575271113e98b9
RiniTheDeveloper <ilya.ilinich@gmail.com>
2023-09-09 19:18:55 +0300

    vue, vue-router, vuetify, pinia added to project

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import { createPinia } from 'pinia'
+import router from './router'
+
+loadFonts()
+
+createApp(App).use(router).use(createPinia())
+  .use(vuetify)
+  .mount('#app')
